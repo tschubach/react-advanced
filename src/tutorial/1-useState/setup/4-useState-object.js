9eb5ab2968202f6f3b762d5342da94a35a72a816
Tim Schubach <tschubach@geico.com>
2021-08-31 08:10:28 -0400

    useState hook - Multiple State Values

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -7,15 +7,21 @@ const UseStateObject = () => {
     message: 'random message'
   })
 
+  // Could also implement separate state values
+  const [name, setName] = useState('peter');
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState('random message');
+
   const changeMessage = () => {
-    setPerson({...person, message: 'hello world' });
+    setPerson({ ...person, message: 'hello world' });
+    setMessage('hello world');
   };
 
   return (
     <>
       <h3>{person.name}</h3>
       <h3>{person.age}</h3>
-      <h3>{person.message}</h3>
+      <h4>{message}</h4>
       <button className='btn' onClick={changeMessage}>change message</button>
     </>
   );
